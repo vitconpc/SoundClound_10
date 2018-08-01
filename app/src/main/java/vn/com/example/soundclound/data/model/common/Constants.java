cacd4141da1ccc1e94e5e544340831198e799e58
DongHien0896 <donghien0896@gmail.com>
2018-08-01 15:19:14 +0700

    init_api

--- app/src/main/java/vn/com/example/soundclound/data/model/common/Constants.java
+++ app/src/main/java/vn/com/example/soundclound/data/model/common/Constants.java
@@ -1,5 +1,9 @@
 package vn.com.example.soundclound.data.model.common;
 
+import vn.com.example.soundclound.BuildConfig;
+
 public class Constants {
+
+    public static final String CLIENT_ID = BuildConfig.CLIENT_ID;
 
 }
