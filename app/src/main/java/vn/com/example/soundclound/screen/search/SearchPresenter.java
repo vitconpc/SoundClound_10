6cb3cf9ff259c5e04c9d436f5a0bf67be759cf12
DongHien0896 <donghien0896@gmail.com>
2018-08-08 14:47:50 +0700

    screen_search

---
+++ app/src/main/java/vn/com/example/soundclound/screen/search/SearchPresenter.java
@@ -0,0 +1,21 @@
+package vn.com.example.soundclound.screen.search;
+
+import vn.com.example.soundclound.screen.base.presenter.BasePresenterImpl;
+
+public class SearchPresenter extends BasePresenterImpl<SearchContract.View> implements SearchContract.Presenter {
+
+    @Override
+    public void setView(SearchContract.View view) {
+
+    }
+
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+}
